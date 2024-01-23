a35592a5d99d68d6e5f5c88d50638dc54f984743
likhnic <likhith26090@gmail.com>
2024-01-23 18:11:54 +0530

    Fixed an error in Task-3

--- Assignment-1/Task-3/load_balancer.py
+++ Assignment-1/Task-3/load_balancer.py
@@ -6,9 +6,9 @@ import logging
 
 app = Quart(__name__)
 PORT = 5000
-currServer = 0
+currServer = 3
 serverList = ["server1", "server2", "server3"]
-nservers = 0
+nservers = 3
 logging.basicConfig(level=logging.DEBUG)
 
 def spawn_server():
