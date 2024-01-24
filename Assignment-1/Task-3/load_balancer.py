8d7fba1c0f6df69be275e2c64f46821ed95e8ae9
shivansh1102 <shivanshshukla60@kgpian.iitkgp.ac.in>
2024-01-24 18:30:53 +0530

    add_container() and some changes in spawn_server()

--- Assignment-1/Task-3/load_balancer.py
+++ Assignment-1/Task-3/load_balancer.py
@@ -28,6 +28,7 @@ def spawn_server(serverName=None):
         return False
     else:
         app.logger.info(f"Spawned {containerName}")
+        map.addServer(serverId=serverId)
         server_to_id[serverName] = serverId
         id_to_server[serverId] = serverName
         nservers += 1
@@ -56,6 +57,7 @@ async def periodic_heatbeat_check(interval=1):
         for serverName,isDown in results:
             if isDown == False:
                 app.logger.error(f"Server {serverName} is down")
+                map.removeServer(serverId=server_to_id[serverName])
                 deadServerList.append(serverName)
         for serverName in deadServerList:
             nservers -= 1
@@ -87,8 +89,22 @@ async def route_to_server(path):
     return jsonify(message=message, status="successful"), 200
 
 @app.route('/add', methods=['POST'])
-def add_container():
-    pass
+def add_container(payload):
+
+    if payload["n"] is None or payload["hostnames"] is None : 
+        return jsonify(message=f"<ERROR> payload doesn't have 'n' or 'hostnames' field", status="failure"), 400
+    if len(payload["hostnames"]) > payload["n"] : 
+        return jsonify(message=f"<ERROR> Length of hostname list is more than newly added instances", status="failure"), 400
+    prev_count = nservers
+    for i in range(payload["n"]) : 
+        serverName = None
+        if i < len(payload["hostnames"]):
+            serverName=payload["hostnames"][i]
+        if serverName not in server_to_id:
+            spawn_server(serverName=serverName)
+    if nservers == prev_count :
+        return jsonify(message=f"<ERROR> Couldn't add server", status="failure"), 400
+    return replicas_list
 
 @app.route('/rm', methods=['DELETE'])
 def remove_container():
